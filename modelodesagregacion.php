54fefa9f3ced341cde3c848fc8b00133bd3f84d1
CrisCFM <ccforerom@academia.usbbog.edu.co>
2022-08-29 19:38:44 -0500

    pagina web

---
+++ modelodesagregacion.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet"  href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp">
+<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+<link rel="stylesheet" href="style2.css">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+     <!--Iconos-->
+    <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<style>
+.icon0{   
+    height: 5vh;
+    } 
+    .icon2{   
+    height: 45vh;
+    } 
+</style>
+<body  style="background-color:#BAFCF1;" >
+    <h1>Patron de desagregación</h1>
+   
+    <h2>Gestion de infraestructuras:</h2>
+        <p align="justify"> en este patron buscamos la creacion y manutencion de una plataforma o alguna instalacion que nos brinde servicio y soporte a altos volumenes de procesos y operaciones o en tareas las cuales se realizen de una manera repetitiva. Ya sea desde la parte logistica, produccion o nuestra plataforma   <p>
+    <br></br>
+    </div>
+    <div align="center">
+    <img  class="icon2" src="img/desagregacion.png"> 
+    </div >
+    <div class="text1" >
+    
+
+
+    <footer class="pie-pagina">
+            <div class="grupo-1">
+                <div class="box">                   
+                            
+
+                            <li class="list__item clase2"   >            
+                                <img  class="icon0" src="img/datos.png">                           
+                                <h5>AdvanceYourCode</h5>                                 
+                            </li> 
+                  
+                </div>
+                <div class="box">
+                    <h2>SOBRE NOSOTROS</h2>
+                    <p>Angel Mauricio Gaitan Mahecha-amgaitanm@academia.usbbog.edu.co</p>
+                    <p>Cristian Camilo Forero Murillo-ccforerom@academia.usbbog.edu.co</p>
+                </div>
+               <!-- <div class="box">
+                    <h2>SIGUENOS</h2>
+                    <div class="red-social">
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-youtube"></a>
+                    </div>
+                </div>-->
+            </div>
+            <div class="grupo-2">
+                <small>&copy; 2022 <b>AdvanceYourCode</b> - Todos los Derechos Reservados.</small>
+            </div>
+        </footer>
+    
+</body>
+</html>
